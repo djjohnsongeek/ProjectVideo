dbefe992686a4f1bbe3c650b02b40cb027f23f34
Daniel Johnson <22306@greensboro-nc.gov>
2025-04-14 15:56:50 -0400

    feat: add dbup

---
+++ ProjectVideo.DatabaseSetup/DatabaseTools.cs
@@ -0,0 +1,75 @@
+﻿using DbUp;
+using DbUp.Engine;
+using DbUp.Support;
+using Microsoft.Data.SqlClient;
+using System.Reflection;
+
+namespace ProjectVideo.DatabaseSetup
+{
+    public class DatabaseTools
+    {
+
+        private readonly string ConnectionString;
+        private readonly string DatabaseName;
+        private readonly string SnapShotName;
+        private readonly string ProjectNamespace = $"{nameof(ProjectVideo)}.{nameof(ProjectVideo.DatabaseSetup)}";
+
+        public DatabaseTools(string connectionString)
+        {
+            ArgumentException.ThrowIfNullOrWhiteSpace(connectionString, nameof(connectionString));
+            ConnectionString = connectionString;
+
+            var conn = new SqlConnection(ConnectionString);
+            DatabaseName = conn.Database;
+            SnapShotName = DatabaseName + "_SnapShot";
+            conn.Dispose();
+        }
+
+        /// <summary>
+        /// Initializes the database, optionally seeds with test data.
+        /// </summary>
+        /// <param name="seedWithTestData"></param>
+        /// <returns></returns>
+        public DatabaseUpgradeResult RunMigrations(bool seedWithTestData = false)
+        {
+            EnsureDatabase.For.SqlDatabase(ConnectionString);
+
+            Assembly executingAssembly = Assembly.GetExecutingAssembly();
+
+            var dbUpgradeEngineBuilder = DeployChanges.To
+                .SqlDatabase(ConnectionString)
+                .JournalToSqlTable("dbo", "_Migrations")
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly(), x => x.StartsWith($"{ProjectNamespace}.Scripts.PreMigration"), new SqlScriptOptions { ScriptType = ScriptType.RunAlways, RunGroupOrder = 0 })
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly(), x => x.StartsWith($"{ProjectNamespace}.Scripts.Migration"), new SqlScriptOptions { ScriptType = ScriptType.RunOnce, RunGroupOrder = 1 })
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly(), x => x.StartsWith($"{ProjectNamespace}.Scripts.PostMigration"), new SqlScriptOptions { ScriptType = ScriptType.RunAlways, RunGroupOrder = 2 })
+                .WithTransactionPerScript()
+                .LogToConsole();
+
+            if (seedWithTestData)
+            {
+                dbUpgradeEngineBuilder = dbUpgradeEngineBuilder.WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly(), x => x.StartsWith($"{ProjectNamespace}.Scripts.TestData"), new SqlScriptOptions { ScriptType = ScriptType.RunAlways, RunGroupOrder = 3 });
+            }
+
+            var upgradeEngine = dbUpgradeEngineBuilder.Build();
+            var scriptsToExecute = upgradeEngine.GetScriptsToExecute();
+            var result = upgradeEngine.PerformUpgrade();
+            var executedScripts = upgradeEngine.GetExecutedScripts();
+            return result;
+        }
+
+
+        private async Task<bool> DatabaseExists(SqlConnection conn, string dbname)
+        {
+            SqlCommand cmd = conn.CreateCommand();
+            cmd.CommandText = "SELECT 1 FROM master.sys.databases WHERE name = @DBName;";
+            cmd.Parameters.AddWithValue("@DBName", dbname);
+
+            // Check if the Database exists
+            var reader = await cmd.ExecuteReaderAsync();
+            bool dbExists = await reader.ReadAsync();
+            await reader.DisposeAsync();
+
+            return dbExists;
+        }
+    }
+}
--- ProjectVideo.DatabaseSetup/Program.cs
+++ ProjectVideo.DatabaseSetup/Program.cs
@@ -1,10 +1,77 @@
-﻿namespace ProjectVideo.DatabaseSetup
+﻿using DbUp.Engine;
+
+namespace ProjectVideo.DatabaseSetup
 {
 	internal class Program
 	{
-		static void Main(string[] args)
+        private const string SEED_ACTION = "seed";
+
+        static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
-		}
-	}
+            // Validate arguments
+            (bool seed, string connStr) = ParseArgs(args);
+            if (string.IsNullOrEmpty(connStr))
+            {
+                Console.WriteLine("No SQL connection string argument found.");
+                Environment.Exit(1);
+            }
+
+            DatabaseTools dbTools = new DatabaseTools(connStr);
+            // Attempt to Migrate the Database
+            try
+            {
+                DatabaseUpgradeResult result = dbTools.RunMigrations(seed);
+                if (result.Successful)
+                {
+                    WriteLine("Database migration successful!", ConsoleColor.Green);
+                    Environment.ExitCode = 0;
+                }
+                else
+                {
+                    WriteLine("Database migration failed.", ConsoleColor.Red);
+                    Environment.ExitCode = 1;
+                }
+            }
+            catch (Exception e)
+            {
+                WriteLine($"An unexpected error occured: {e.Message}");
+                WriteLine("--- Stack Trace ---");
+                WriteLine(e.StackTrace);
+                Environment.ExitCode = 1;
+            }
+        }
+
+        private static void WriteLine(string? message, ConsoleColor? color = null)
+        {
+            if (color.HasValue)
+            {
+                Console.ForegroundColor = color.Value;
+            }
+
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+
+        private static (bool seed, string connectionString) ParseArgs(string[] args)
+        {
+            (bool seed, string connStr) result = (false, "");
+
+            if (args.Length == 1 || args.Length == 2)
+            {
+                foreach (string arg in args)
+                {
+                    if (arg == SEED_ACTION)
+                    {
+                        result.seed = true;
+                    }
+                    else if (arg.Contains("Server=") || arg.Contains("Data Source="))
+                    {
+                        result.connStr = arg;
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
 }
