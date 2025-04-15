1d45c05bb167ca7f8dbe25d6ee12388ed855232a
Daniel Johnson <danieleejohnson@gmail.com>
2025-04-15 04:41:11 -0400

    feat: staring working on front end

---
+++ ProjectVideo.Web/wwwroot/js/CollectionInput.js
@@ -0,0 +1,19 @@
+﻿class CollectionInput {
+    count = 1;
+    fieldNames = [];
+    baseName = "";
+    container = null;
+
+    constructor(containerId, baseName, fieldNames) {
+        this.container = document.getElementById(containerId);
+        this.fieldNames = fieldNames;
+        this.baseName = baseName;
+    }
+
+    renderInputs() {
+        for (let i = 0; i < this.count; i++)
+        {
+
+        }
+    }
+}
---
+++ ProjectVideo.Web/wwwroot/js/ViewScripts/Proposals/Form.js
@@ -0,0 +1,14 @@
+﻿class ProposalFormPage {
+
+    membersInputContainer;
+    memnersInputAddBtn
+
+    constructor() {
+        this.membersInputContainer = document.getElementById("members-input-container");
+        this.memnersInputAddBtn = document.getElementById("members-input-add-btn");
+
+        this.memnersInputAddBtn.addEventListener("click", (event) => {
+
+        })
+    }
+}
