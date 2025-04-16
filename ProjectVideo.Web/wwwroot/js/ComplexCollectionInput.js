e23b88f5d03ec5ba659479a4532adf9123a662c1
Daniel Johnson <danieleejohnson@gmail.com>
2025-04-16 05:19:14 -0400

    feat: add complex collection binding

---
+++ ProjectVideo.Web/wwwroot/js/ComplexCollectionInput.js
@@ -0,0 +1,54 @@
+﻿class ComplexCollectionInput {
+    count = 1;
+    propertyNames = [];
+    labels = [];
+    objectName = "";
+    collectionContainer = null;
+
+    constructor(containerId, objectName, propertyNames, labels) {
+        if (labels.length != propertyNames.length) {
+            throw new Error("The Properties and Label arrays have different lengths.");
+        }
+
+        this.collectionContainer = document.getElementById(containerId);
+        this.propertyNames = propertyNames;
+        this.objectName = objectName;
+        this.labels = labels;
+    }
+
+    updateCount(n) {
+        this.count += n;
+        this.renderInputs();
+    }
+
+    renderInputs() {
+        const rows = [];
+        for (let i = 0; i < this.count; i++)
+        {
+            const containerRow = document.createElement("div");
+            containerRow.className = "row";
+
+            for (let j = 0; j < this.labels.length; j++) {
+                const containerCol = document.createElement("div");
+                containerCol.className = "col-6";
+
+                const label = document.createElement("label");
+                label.className = "form-label";
+                const labelText = document.createTextNode(this.labels[j]);
+                label.appendChild(labelText);
+
+                const input = document.createElement("input");
+                input.className = "form-control";
+                input.name = `${this.objectName}[${i}].${this.propertyNames[j]}`;
+
+                containerCol.appendChild(label);
+                containerCol.appendChild(input);
+                containerRow.appendChild(containerCol);
+            }
+
+            rows.push(containerRow);
+        }
+
+        this.collectionContainer.replaceChildren(...rows);
+    }
+}
--- ProjectVideo.Web/wwwroot/js/ViewScripts/Proposals/Form.js
+++ ProjectVideo.Web/wwwroot/js/ViewScripts/Proposals/Form.js
@@ -1,14 +1,40 @@
 ﻿class ProposalFormPage {
+    membersInputAddBtn;
+    membersComplexCollectionInput;
 
-    membersInputContainer;
-    memnersInputAddBtn
+    linksInputAddBtn;
+    linksComplexCollectionInput;
 
     constructor() {
-        this.membersInputContainer = document.getElementById("members-input-container");
-        this.memnersInputAddBtn = document.getElementById("members-input-add-btn");
+        this.membersInputAddBtn = document.getElementById("members-input-add-btn");
+        this.linksInputAddBtn = document.getElementById("links-input-add-btn");
 
-        this.memnersInputAddBtn.addEventListener("click", (event) => {
+        this.membersComplexCollectionInput = new ComplexCollectionInput(
+            "members-input-container",
+            "TeamMembers",
+            ["Name", "Role"],
+            ["Name", "Role"]
+        );
 
-        })
+        this.linksComplexCollectionInput = new ComplexCollectionInput(
+            "links-input-container",
+            "VideoLinks",
+            ["Name", "Url"],
+            ["Link Name", "Link URL"]
+        )
+
+        this.membersInputAddBtn.addEventListener("click", (event) => {
+            this.membersComplexCollectionInput.updateCount(1);
+        });
+        this.linksInputAddBtn.addEventListener("click", (event) => {
+            this.linksComplexCollectionInput.updateCount(1);
+        });
+
+        this.linksInputContainer = document.getElementById("");
+
+        this.membersComplexCollectionInput.renderInputs();
+        this.linksComplexCollectionInput.renderInputs();
     }
 }
+
+var proposalForm = new ProposalFormPage();
