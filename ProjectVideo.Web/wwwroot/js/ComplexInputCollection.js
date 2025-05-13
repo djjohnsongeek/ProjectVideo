cbdeae44d9a53562fb8e11c84ae6a7a9bf357570
Daniel Johnson <22306@greensboro-nc.gov>
2025-05-13 08:21:03 -0400

    feat: get complext input collections working

---
+++ ProjectVideo.Web/wwwroot/js/ComplexInputCollection.js
@@ -0,0 +1,110 @@
+﻿class ComplexInputCollection {
+    collectionContainer;
+    inputGroups;
+    constructor(containerId) {
+        this.collectionContainer = document.getElementById(containerId);
+        this.inputGroups = [];
+    }
+    addInputGroup(inputGroup) {
+        this.inputGroups.push(inputGroup);
+        this.renderInputs();
+    }
+    handleInputValueChanges(event) {
+        const input = event.currentTarget;
+        const guidId = input.id;
+        this.setInputValue(input.value, guidId);
+    }
+    handleInputRemoveBtn(event) {
+        const input = event.currentTarget;
+        const guidId = input.dataset.deleteTarget;
+        this.inputGroups = this.inputGroups.filter((item) => {
+            return item.guidId != guidId;
+        });
+        this.renderInputs();
+    }
+    setInputValue(value, guidId) {
+        let complexInput = this.getInputById(guidId);
+        if (complexInput !== null) {
+            complexInput.inputValue = value;
+        }
+        else {
+            console.error("Failed to set input value. No input data found.");
+        }
+    }
+    getInputById(guidId) {
+        for (let inputGroup of this.inputGroups) {
+            for (let complexInput of inputGroup.inputs) {
+                if (complexInput.elementId == guidId) {
+                    return complexInput;
+                }
+            }
+        }
+        return null;
+    }
+    renderInputs() {
+        const rows = [];
+        for (let i = 0; i < this.inputGroups.length; i++) {
+            const containerRow = document.createElement("div");
+            containerRow.className = "row mt-3";
+            for (let j = 0; j < this.inputGroups[i].inputs.length; j++) {
+                const containerCol = document.createElement("div");
+                containerCol.className = "col-5";
+                const label = document.createElement("label");
+                label.className = "form-label";
+                const labelText = document.createTextNode(this.inputGroups[i].inputs[j].labelName);
+                label.appendChild(labelText);
+                const input = document.createElement("input");
+                input.className = "form-control";
+                input.name = `${this.inputGroups[i].objectName}[${i}].${this.inputGroups[i].inputs[j].propertyName}`;
+                input.id = this.inputGroups[i].inputs[j].elementId;
+                input.value = this.inputGroups[i].inputs[j].inputValue;
+                if (this.inputGroups[i].inputs[j].inputDatalistId !== null) {
+                    input.setAttribute("list", this.inputGroups[i].inputs[j].inputDatalistId);
+                }
+                input.addEventListener("change", (event) => this.handleInputValueChanges(event));
+                containerCol.appendChild(label);
+                containerCol.appendChild(input);
+                containerRow.appendChild(containerCol);
+            }
+            const deleteCol = document.createElement("div");
+            deleteCol.className = "col-2";
+            const deleteInput = document.createElement("input");
+            deleteInput.className = "btn btn-outline-danger form-control";
+            deleteInput.type = "button";
+            deleteInput.value = "Remove";
+            deleteInput.dataset.deleteTarget = this.inputGroups[i].guidId;
+            deleteInput.addEventListener("click", (event) => this.handleInputRemoveBtn(event));
+            const deleteBtnLabel = document.createElement("label");
+            deleteBtnLabel.appendChild(document.createTextNode("Remove Url"));
+            deleteBtnLabel.className = "form-label";
+            deleteBtnLabel.style.visibility = "hidden";
+            deleteCol.appendChild(deleteBtnLabel);
+            deleteCol.appendChild(deleteInput);
+            containerRow.appendChild(deleteCol);
+            rows.push(containerRow);
+        }
+        this.collectionContainer.replaceChildren(...rows);
+    }
+}
+
+class ComplexInput {
+    constructor(labelName, propertyName, elementId, inputValue, inputDatalistId = null) {
+        this.labelName = labelName;
+        this.propertyName = propertyName;
+        this.elementId = elementId;
+        this.inputValue = inputValue;
+        this.inputDatalistId = inputDatalistId;
+    }
+}
+
+class ComplexObjectInputGroup {
+    constructor(objectName, guidId) {
+        this.objectName = objectName;
+        this.guidId = guidId;
+        this.inputs = [];
+    }
+
+    addInput(complexInput) {
+        this.inputs.push(complexInput);
+    }
+}
--- ProjectVideo.Web/wwwroot/js/ViewScripts/Proposals/Form.js
+++ ProjectVideo.Web/wwwroot/js/ViewScripts/Proposals/Form.js
@@ -1,39 +1,58 @@
 ﻿class ProposalFormPage {
     membersInputAddBtn;
-    membersComplexCollectionInput;
+    membersComplexInputCollection;
 
     linksInputAddBtn;
-    linksComplexCollectionInput;
+    linksComplexInputCollection;
+
+    roleNameDatalistId = "rolename-options";
+    membersInputContainerId = "members-input-container";
+    videoLinksInputContainerId = "links-input-container";
+    linksInputContainerId = "";
 
     constructor() {
         this.membersInputAddBtn = document.getElementById("members-input-add-btn");
         this.linksInputAddBtn = document.getElementById("links-input-add-btn");
 
-        this.membersComplexCollectionInput = new ComplexCollectionInput(
-            "members-input-container",
-            "TeamMembers",
-            ["Name", "Role"],
-            ["Name", "Role"]
-        );
-
-        this.linksComplexCollectionInput = new ComplexCollectionInput(
-            "links-input-container",
-            "VideoLinks",
-            ["Name", "Url"],
-            ["Link Name", "Link URL"]
-        )
-
+        // Members inputs
+        this.membersComplexInputCollection = new ComplexInputCollection(this.membersInputContainerId);
+        this.membersComplexInputCollection.addInputGroup(this.buildDefaultMemberInputGroup());
         this.membersInputAddBtn.addEventListener("click", (event) => {
-            this.membersComplexCollectionInput.updateCount(1);
+            this.membersComplexInputCollection.addInputGroup(this.buildDefaultMemberInputGroup());
         });
+
+        // Video Links inputs
+        this.videoLinkComplexInputCollection = new ComplexInputCollection(this.videoLinksInputContainerId);
+        this.videoLinkComplexInputCollection.addInputGroup(this.buildDefaultLinkInputGroup());
         this.linksInputAddBtn.addEventListener("click", (event) => {
-            this.linksComplexCollectionInput.updateCount(1);
+            this.videoLinkComplexInputCollection.addInputGroup(this.buildDefaultLinkInputGroup());
         });
+    }
 
-        this.linksInputContainer = document.getElementById("");
+    buildDefaultMemberInputGroup() {
+        const inputGroup = new ComplexObjectInputGroup("TeamMembers", crypto.randomUUID());
+        const complexInputs = [
+            new ComplexInput("Name", "Name", crypto.randomUUID(), ""),
+            new ComplexInput("Role", "Role", crypto.randomUUID(), "", this.roleNameDatalistId)
+        ]
+        for (let input of complexInputs) {
+            inputGroup.addInput(input);
+        }
 
-        this.membersComplexCollectionInput.renderInputs();
-        this.linksComplexCollectionInput.renderInputs();
+        return inputGroup;
+    }
+
+    buildDefaultLinkInputGroup() {
+        const inputGroup = new ComplexObjectInputGroup("VideoLinks", crypto.randomUUID());
+        const complexInputs = [
+            new ComplexInput("Name", "Name", crypto.randomUUID(), ""),
+            new ComplexInput("Url", "Url", crypto.randomUUID(), "")
+        ]
+        for (let input of complexInputs) {
+            inputGroup.addInput(input);
+        }
+
+        return inputGroup;
     }
 }
 
