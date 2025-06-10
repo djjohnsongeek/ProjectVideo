de4ef07ee8bce0802971914d6fa9c0c5760cf4a0
Daniel Johnson <danieleejohnson@gmail.com>
2025-06-10 02:22:01 -0400

    feat: improve form display

--- ProjectVideo.Web/wwwroot/js/ComplexInputCollection.js
+++ ProjectVideo.Web/wwwroot/js/ComplexInputCollection.js
@@ -45,10 +45,10 @@
         const rows = [];
         for (let i = 0; i < this.inputGroups.length; i++) {
             const containerRow = document.createElement("div");
-            containerRow.className = "row mt-3";
+            containerRow.className = "row mb-3 pt-1 pb-4 border-bottom border-2";
             for (let j = 0; j < this.inputGroups[i].inputs.length; j++) {
                 const containerCol = document.createElement("div");
-                containerCol.className = "col-5";
+                containerCol.className = "col-md-5";
                 const label = document.createElement("label");
                 label.className = "form-label";
                 const labelText = document.createTextNode(this.inputGroups[i].inputs[j].labelName);
@@ -67,7 +67,7 @@
                 containerRow.appendChild(containerCol);
             }
             const deleteCol = document.createElement("div");
-            deleteCol.className = "col-2";
+            deleteCol.className = "col-md-2";
             const deleteInput = document.createElement("input");
             deleteInput.className = "btn btn-outline-danger form-control";
             deleteInput.type = "button";
